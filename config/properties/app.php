1ae5ef393917bf44e1c5316d53276763b8225ecb
remon <15856943206@163.com>
2019-02-22 17:31:50 +0800

    开启swoft dev

--- config/properties/app.php
+++ config/properties/app.php
@@ -15,6 +15,7 @@ return [
     'bootScan'     => [
         'App\Commands',
         'App\Boot',
+        'App\WebSocket',
     ],
     'excludeScan'  => [
 
@@ -27,4 +28,13 @@ return [
     'service'      => require __DIR__ . DS . 'service.php',
     'breaker'      => require __DIR__ . DS . 'breaker.php',
     'provider'     => require __DIR__ . DS . 'provider.php',
+
+    'devtool' => [
+        // 是否开启 DevTool，默认值为 false
+        'enable' => true,
+        // (可选)前台运行服务器时，是否打印事件调用到 Console
+        'logEventToConsole' => true,
+        // (可选)前台运行服务器时，是否打印 HTTP 请求到 Console
+        'logHttpRequestToConsole' => true,
+    ],
 ];
