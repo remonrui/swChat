64e3e84509e151f292969f0398cbe4ccc1e6dea2
remon <15856943206@163.com>
2019-02-22 17:33:13 +0800

    文件删除

--- app/WebSocket/ChatController.php
+++ app/WebSocket/ChatController.php
@@ -1,20 +1,20 @@
 <?php
-
 namespace App\WebSocket;
-
+use App\Lib\DataFormat;
+use App\Lib\MsgCode;
+use Swoft\Helper\JsonHelper;
 use Swoft\Http\Message\Server\Request;
 use Swoft\Http\Message\Server\Response;
 use Swoft\WebSocket\Server\Bean\Annotation\WebSocket;
 use Swoft\WebSocket\Server\HandlerInterface;
 use Swoole\WebSocket\Frame;
 use Swoole\WebSocket\Server;
-
 /**
  * Class EchoController
  * @package App\WebSocket
- * @WebSocket("/echo")
+ * @WebSocket("/chat")
  */
-class EchoController implements HandlerInterface
+class ChatController implements HandlerInterface
 {
     /**
      * {@inheritdoc}
@@ -23,7 +23,6 @@ class EchoController implements HandlerInterface
     {
         return [0, $response];
     }
-
     /**
      * @param Server $server
      * @param Request $request
@@ -33,16 +32,30 @@ class EchoController implements HandlerInterface
     {
         $server->push($fd, 'hello, welcome! :)');
     }
-
     /**
      * @param Server $server
      * @param Frame $frame
      */
     public function onMessage(Server $server, Frame $frame)
     {
-        $server->push($frame->fd, 'hello, I have received your message: ' . $frame->data);
-    }
+        //$server->push($frame->fd, 'hello, I have received your message: ' . $frame->data);
 
+        $fd = $frame->fd;
+
+        try{
+            $data = JsonHelper::decode($frame->data);
+
+            $userName = 'user' . $fd;
+
+
+
+
+        }catch (\Exception $E){
+
+            $server->push($fd,DataFormat::show(MsgCode::$paramsError));
+
+        }
+    }
     /**
      * @param Server $server
      * @param int $fd
